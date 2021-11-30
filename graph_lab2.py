57787707ac16cd963c5aa3052d8c6f46bb7877f0
shramkodiana <72132936+shramkodiana@users.noreply.github.com>
2021-11-30 19:03:34 +0200

    Add files via upload

---
+++ graph_lab2.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageColor, ImageDraw
+import pandas as pd
+
+width = 960
+height = 540
+image = Image.new("RGB", (width, height), (255, 255, 255))
+filepath = input("Введіть шлях до файлу: ")
+df = pd.read_csv(filepath, sep=" ", header=None)
+h = ['x', 'y']
+df.columns = h
+
+drawing = ImageDraw.Draw(image)
+for index, row in df.iterrows():
+    x, y = row['y'], 540 - row['x']
+    drawing.point((x, y), fill=ImageColor.getrgb("black"))
+image.show()
+name = input("Введіть назву для збереженої картинки(у вигляді 'file_name.png'):")
+image.save(name, "PNG")
